903dab1ebbf3cb2a9cc2f9aebfb2fbe8f6722079
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-21 08:32:39 +0300

    first commit

--- src/AnimalPage.js
+++ src/AnimalPage.js
@@ -1,7 +1,8 @@
+
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function AnimalPage() {
   const [animals, setAnimals] = useState([]);
   const [selectedAnimal, setSelectedAnimal] = useState(null);
 
@@ -52,4 +53,4 @@ function App() {
   );
 }
 
-export default App;
+export default AnimalPage;
